570067d860a67715d0c249360c674b67db1952c8
David Kalmus <dejvios@seznam.cz>
2025-07-04 13:06:13 +0200

    vue-3-TS

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  TODO: APP.VUE
+  <main>
+    <h1>{{ message }}</h1>
+    
+    <form>
+      <label>
+        New Task
+        <input type="text" v-model="newTask">
+      </label>
+    </form>
+  </main>
 </template>
 
 <script setup lang="ts">
+  import { ref } from 'vue';
 
+  const message = ref<string>('Tasks App');
+  const newTask = ref<string>("");
 </script>
 
 <style scoped>
-
+  main {
+    max-width: 800px;
+    margin: 1rem auto;
+  }
 </style>
